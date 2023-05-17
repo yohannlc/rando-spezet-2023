263afeab1a2591e90b529941f7f6888f192da018
yohannlc <yohann.lecamx@gmail.com>
2023-05-17 21:00:33 +0200

    Add popup when page is loaded

--- js/mapbox-example.js
+++ js/mapbox-example.js
@@ -1,8 +1,18 @@
-onload = function() {
-  this.alert('Dernière mise à jour : 17/05/2023');
-};
+window.addEventListener('DOMContentLoaded', function() {
+  var popup = document.getElementById('popup');
+  var closeButton = popup.querySelector('.close');
 
+  function hidePopup() {
+    popup.style.opacity = '0';
+    setTimeout(function() {
+      popup.style.display = 'none';
+    }, 500); // Durée de transition définie dans la propriété "transition" en CSS (0.5s)
+  }
 
+  popup.style.display = 'block';
+  closeButton.addEventListener('click', hidePopup);
+  setTimeout(hidePopup, 8000);
+});
 
 /* 
 Types d'affichage disponibles :
